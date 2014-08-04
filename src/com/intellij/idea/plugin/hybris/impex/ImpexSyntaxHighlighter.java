8b759b41e84b670447675c2d0f34f6fe17bcf7a2
Alexander Bartash <AlexanderBartash@gmail.com>
2014-08-04 19:18:14 +0300

    Colours fixed. Required idea build version updated to 13.1.

--- src/com/intellij/idea/plugin/hybris/impex/ImpexSyntaxHighlighter.java
+++ src/com/intellij/idea/plugin/hybris/impex/ImpexSyntaxHighlighter.java
@@ -18,12 +18,12 @@ import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAtt
 
 public class ImpexSyntaxHighlighter extends SyntaxHighlighterBase {
     public static final TextAttributesKey COMMENT = createTextAttributesKey("IMPEX_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    public static final TextAttributesKey SEMICOLON = createTextAttributesKey("IMPEX_SEMICOLON", new TextAttributes(JBColor.decode("#C96B0E"), null, null, null, Font.BOLD));
-    public static final TextAttributesKey COMMA = createTextAttributesKey("IMPEX_COMMA", new TextAttributes(JBColor.decode("#1C9C0A"), null, null, null, Font.BOLD));
-    public static final TextAttributesKey ROUND_BRACKETS = createTextAttributesKey("IMPEX_ROUND_BRACKETS", new TextAttributes(JBColor.decode("#376DCF"), null, null, null, Font.BOLD));
-    public static final TextAttributesKey SQUARE_BRACKETS = createTextAttributesKey("IMPEX_SQUARE_BRACKETS", new TextAttributes(JBColor.decode("#783FCF"), null, null, null, Font.BOLD));
-    public static final TextAttributesKey INSERT_UPDATE = createTextAttributesKey("IMPEX_INSERT_UPDATE", new TextAttributes(JBColor.decode("#783FCF"), null, null, null, Font.BOLD));
-    public static final TextAttributesKey TABLE_NAME = createTextAttributesKey("IMPEX_TABLE_NAME", new TextAttributes(JBColor.decode("#775AD4"), null, null, null, Font.BOLD));
+    public static final TextAttributesKey SEMICOLON = createTextAttributesKey("IMPEX_SEMICOLON", new TextAttributes(JBColor.decode("#B85C0E"), null, null, null, Font.BOLD));
+    public static final TextAttributesKey COMMA = createTextAttributesKey("IMPEX_COMMA", new TextAttributes(JBColor.decode("#B85C0E"), null, null, null, Font.BOLD));
+    public static final TextAttributesKey ROUND_BRACKETS = createTextAttributesKey("IMPEX_ROUND_BRACKETS", new TextAttributes(JBColor.decode("#B5AA1B"), null, null, null, Font.BOLD));
+    public static final TextAttributesKey SQUARE_BRACKETS = createTextAttributesKey("IMPEX_SQUARE_BRACKETS", new TextAttributes(JBColor.decode("#B85C0E"), null, null, null, Font.BOLD));
+    public static final TextAttributesKey INSERT_UPDATE = createTextAttributesKey("IMPEX_INSERT_UPDATE", new TextAttributes(JBColor.decode("#B85C0E"), null, null, null, Font.BOLD));
+    public static final TextAttributesKey TABLE_NAME = createTextAttributesKey("IMPEX_TABLE_NAME", new TextAttributes(JBColor.decode("#978D19"), null, null, null, Font.BOLD));
 
     //static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey("IMPEX_BAD_CHARACTER", new TextAttributes(Color.RED, null, null, null, Font.BOLD));
     static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey("IMPEX_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
