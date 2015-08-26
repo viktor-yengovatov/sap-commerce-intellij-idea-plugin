ccd1459e811a18cdb37802d6b54019dbf6962184
i303703 <martin.zdarsky@hybris.com>
2015-08-27 08:38:17 +1000

    hybris has to be spelled with low case (not Hybris) already existing modules message was too long bin directory excluded from junk files
    Signed-off-by: Martin Zdarsky martin.zdarsky@hybris.com

---
+++ src/com/intellij/idea/plugin/hybris/project/settings/HybrisModuleDescriptorFactoryImpl.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of "Hybris Integration" plugin for Intellij IDEA.
+ * Copyright (C) 2014-2015 Alexander Bartash <AlexanderBartash@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.intellij.idea.plugin.hybris.project.settings;
+
+import com.intellij.idea.plugin.hybris.project.exceptions.HybrisConfigurationException;
+import com.intellij.idea.plugin.hybris.project.utils.HybrisProjectUtils;
+import org.apache.commons.lang3.Validate;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * Created 1:58 PM 20 June 2015.
+ *
+ * @author Alexander Bartash <AlexanderBartash@gmail.com>
+ */
+public class HybrisModuleDescriptorFactoryImpl implements HybrisModuleDescriptorFactory {
+
+    public static final HybrisModuleDescriptorFactory INSTANCE = new HybrisModuleDescriptorFactoryImpl();
+
+    protected HybrisModuleDescriptorFactoryImpl() {
+    }
+
+    @NotNull
+    @Override
+    public HybrisModuleDescriptor createDescriptor(@NotNull final File file,
+                                                   @NotNull final HybrisProjectDescriptor rootProjectDescriptor
+    ) throws HybrisConfigurationException {
+        Validate.notNull(file);
+        Validate.notNull(rootProjectDescriptor);
+
+        if (HybrisProjectUtils.isConfigModule(file)) {
+            return new ConfigHybrisModuleDescriptor(file, rootProjectDescriptor);
+        }
+
+        if (HybrisProjectUtils.isPlatformModule(file)) {
+            return new PlatformHybrisModuleDescriptor(file, rootProjectDescriptor);
+        }
+
+        return new DefaultHybrisModuleDescriptor(file, rootProjectDescriptor);
+    }
+
+}
---
+++ src/com/intellij/idea/plugin/hybris/settings/HybrisIntegrationSettingsData.java
@@ -0,0 +1,152 @@
+/*
+ * This file is part of "Hybris Integration" plugin for Intellij IDEA.
+ * Copyright (C) 2014-2015 Alexander Bartash <AlexanderBartash@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.intellij.idea.plugin.hybris.settings;
+
+import com.google.common.collect.Lists;
+import com.intellij.ide.util.PropertyName;
+
+import java.util.List;
+
+public class HybrisIntegrationSettingsData {
+
+    public static final List<String> defaultJunkFileNames = Lists.newArrayList(
+        ".classpath",
+        ".directory",
+        ".externalToolBuilders",
+        ".idea",
+        ".pmd",
+        ".project",
+        ".ruleset",
+        ".settings",
+        ".springBeans",
+        "addonsrc",
+        "beans.xsd",
+        "classes",
+        "commonwebsrc",
+        "eclipsebin",
+        "extensioninfo.xsd",
+        "items.xsd",
+        "platformhome.properties",
+        "ruleset.xml",
+        "testclasses"
+    );
+
+    @PropertyName("foldingEnabled")
+    private boolean foldingEnabled = true;
+
+    @PropertyName("useSmartFolding")
+    private boolean useSmartFolding = true;
+
+    @PropertyName("limitedSpringConfig")
+    private boolean limitedSpringConfig = true;
+
+    @PropertyName("groupModules")
+    private boolean groupModules = true;
+
+    @PropertyName("junkDirectoryList")
+    private List<String> junkDirectoryList = defaultJunkFileNames;
+
+    @PropertyName("groupHybris")
+    private String groupHybris = "Hybris";
+
+    @PropertyName("groupOtherHybris")
+    private String groupOtherHybris = "Hybris/Unused";
+
+    @PropertyName("groupCustom")
+    private String groupCustom = "Custom";
+
+    @PropertyName("groupOtherCustom")
+    private String groupOtherCustom = "Custom/Unused";
+
+
+    public HybrisIntegrationSettingsData() {
+    }
+
+    public boolean isFoldingEnabled() {
+        return foldingEnabled;
+    }
+
+    public void setFoldingEnabled(final boolean foldingEnabled) {
+        this.foldingEnabled = foldingEnabled;
+    }
+
+    public boolean isUseSmartFolding() {
+        return useSmartFolding;
+    }
+
+    public void setUseSmartFolding(final boolean foldingEnabled) {
+        this.useSmartFolding = foldingEnabled;
+    }
+
+    public boolean isLimitedSpringConfig() {
+        return limitedSpringConfig;
+    }
+
+    public void setLimitedSpringConfig(final boolean limitedSpringConfig) {
+        this.limitedSpringConfig = limitedSpringConfig;
+    }
+
+    public List<String> getJunkDirectoryList() {
+        return junkDirectoryList;
+    }
+
+    public void setJunkDirectoryList(List<String> junkDirectoryList) {
+        this.junkDirectoryList = junkDirectoryList;
+    }
+
+    public void setGroupModules(final boolean groupModules) {
+        this.groupModules = groupModules;
+    }
+
+    public boolean isGroupModules() {
+        return groupModules;
+    }
+
+    public String getGroupHybris() {
+        return groupHybris;
+    }
+
+    public void setGroupHybris(final String groupHybris) {
+        this.groupHybris = groupHybris;
+    }
+
+    public String getGroupOtherHybris() {
+        return groupOtherHybris;
+    }
+
+    public void setGroupOtherHybris(final String groupOtherHybris) {
+        this.groupOtherHybris = groupOtherHybris;
+    }
+
+    public String getGroupCustom() {
+        return groupCustom;
+    }
+
+    public void setGroupCustom(final String groupCustom) {
+        this.groupCustom = groupCustom;
+    }
+
+    public String getGroupOtherCustom() {
+        return groupOtherCustom;
+    }
+
+    public void setGroupOtherCustom(final String groupOtherCustom) {
+        this.groupOtherCustom = groupOtherCustom;
+    }
+}
