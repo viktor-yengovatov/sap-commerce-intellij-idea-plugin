2244eb60042ec8b38cb05a4efa882bde0937972f
Martin Zdarsky <zdary@zdary.cz>
2016-07-09 20:42:25 +1000

    IIP-92 disable framework detection
    Signed-off-by: Martin Zdarsky-Jones martin.zdarsky@hybris.com

--- src/com/intellij/idea/plugin/hybris/project/DefaultHybrisProjectImportBuilder.java
+++ src/com/intellij/idea/plugin/hybris/project/DefaultHybrisProjectImportBuilder.java
@@ -18,7 +18,13 @@
 
 package com.intellij.idea.plugin.hybris.project;
 
+import com.intellij.facet.FacetConfiguration;
+import com.intellij.facet.FacetType;
+import com.intellij.facet.FacetTypeRegistry;
 import com.intellij.facet.ModifiableFacetModel;
+import com.intellij.framework.FrameworkType;
+import com.intellij.framework.detection.DetectionExcludesConfiguration;
+import com.intellij.framework.detection.impl.FrameworkDetectionUtil;
 import com.intellij.idea.plugin.hybris.common.services.VirtualFileSystemService;
 import com.intellij.idea.plugin.hybris.project.configurators.JavadocModuleConfigurator;
 import com.intellij.idea.plugin.hybris.project.configurators.impl.DefaultConfiguratorFactory;
@@ -39,6 +45,9 @@ import com.intellij.idea.plugin.hybris.settings.HybrisProjectSettingsComponent;
 import com.intellij.idea.plugin.hybris.common.HybrisConstants;
 import com.intellij.idea.plugin.hybris.common.utils.HybrisI18NBundleUtils;
 import com.intellij.idea.plugin.hybris.common.utils.HybrisIcons;
+import com.intellij.javaee.application.facet.JavaeeApplicationFacet;
+import com.intellij.javaee.facet.JavaeeFacet;
+import com.intellij.javaee.web.facet.WebFacet;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
@@ -65,7 +74,11 @@ import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable;
 import com.intellij.openapi.roots.ui.configuration.projectRoot.StructureConfigurableContext;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.packaging.artifacts.ModifiableArtifactModel;
+import com.intellij.spring.facet.SpringFacet;
+import com.intellij.spring.facet.SpringFacetConfiguration;
+import com.intellij.spring.facet.SpringLibraryFrameworkType;
 import com.intellij.util.Function;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
@@ -201,6 +214,8 @@ public class DefaultHybrisProjectImportBuilder extends AbstractHybrisProjectImpo
 
         this.saveCustomDirectoryLocation(project);
 
+        this.excludeFrameworksDetection(project);
+
         this.performProjectsCleanup(this.getHybrisProjectDescriptor().getModulesChosenForImport());
 
         final ModifiableModuleModel rootProjectModifiableModel = (null == model)
@@ -268,6 +283,32 @@ public class DefaultHybrisProjectImportBuilder extends AbstractHybrisProjectImpo
         springConfigurator.configureDependencies(this.getHybrisProjectDescriptor(), rootProjectModifiableModel);
 
         return result;
+    }
+
+    private void excludeFrameworksDetection(final Project project) {
+        final DetectionExcludesConfiguration configuration = DetectionExcludesConfiguration.getInstance(project);
+
+        //Spring
+        final FacetType<SpringFacet, SpringFacetConfiguration> springFacetType = SpringFacet.getSpringFacetType();
+        final FrameworkType springFrameworkType = FrameworkDetectionUtil.findFrameworkTypeForFacetDetector(
+            springFacetType
+        );
+        configuration.addExcludedFramework(springFrameworkType);
+
+        //Web
+        final FacetType<WebFacet, FacetConfiguration> webFacetType = FacetTypeRegistry.getInstance().findFacetType(
+            WebFacet.ID
+        );
+        final FrameworkType webFrameworkType = FrameworkDetectionUtil.findFrameworkTypeForFacetDetector(webFacetType);
+        configuration.addExcludedFramework(webFrameworkType);
+
+        //JavaEE
+        final FacetType<JavaeeApplicationFacet, FacetConfiguration> javaeeApplicationFacetType =
+            FacetTypeRegistry.getInstance().findFacetType(JavaeeApplicationFacet.ID);
+        final FrameworkType javaeeApplicationFrameworkType = FrameworkDetectionUtil.findFrameworkTypeForFacetDetector(
+            javaeeApplicationFacetType
+        );
+        configuration.addExcludedFramework(javaeeApplicationFrameworkType);
     }
 
     private void saveCustomDirectoryLocation(final Project project) {
