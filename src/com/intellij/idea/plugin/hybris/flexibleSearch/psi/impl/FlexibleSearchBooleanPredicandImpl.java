64636bf685c726f71d318fec9f15de3449411635
Aleksandr Nosov <nosovae.dev@gmail.com>
2017-03-21 18:54:59 +0300

    IIP-223 Add basic support of FlexibleSearch. Signed-off-by: Aleksandr Nosov <nosovae.dev@gmail.com>

---
+++ src/com/intellij/idea/plugin/hybris/flexibleSearch/psi/impl/FlexibleSearchBooleanPredicandImpl.java
@@ -0,0 +1,41 @@
+// This is a generated file. Not intended for manual editing.
+package com.intellij.idea.plugin.hybris.flexibleSearch.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.intellij.idea.plugin.hybris.flexibleSearch.psi.FlexibleSearchTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.idea.plugin.hybris.flexibleSearch.psi.*;
+
+public class FlexibleSearchBooleanPredicandImpl extends ASTWrapperPsiElement implements FlexibleSearchBooleanPredicand {
+
+  public FlexibleSearchBooleanPredicandImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull FlexibleSearchVisitor visitor) {
+    visitor.visitBooleanPredicand(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof FlexibleSearchVisitor) accept((FlexibleSearchVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public FlexibleSearchColumnReference getColumnReference() {
+    return findChildByClass(FlexibleSearchColumnReference.class);
+  }
+
+  @Override
+  @Nullable
+  public FlexibleSearchParenthesizedBooleanValueExpression getParenthesizedBooleanValueExpression() {
+    return findChildByClass(FlexibleSearchParenthesizedBooleanValueExpression.class);
+  }
+
+}
