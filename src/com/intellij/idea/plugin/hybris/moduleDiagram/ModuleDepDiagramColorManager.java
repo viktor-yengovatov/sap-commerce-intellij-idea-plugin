77aeac97f3a98b4bd77dae221df05841a0277f8b
Martin Zdarsky <zdary@zdary.cz>
2017-06-06 16:56:27 +1000

    IIP-268 detect circular dependency Signed-off-by: Martin Zdarsky-Jones martin.zdarsky@hybris.com

--- src/com/intellij/idea/plugin/hybris/moduleDiagram/ModuleDepDiagramColorManager.java
+++ src/com/intellij/idea/plugin/hybris/moduleDiagram/ModuleDepDiagramColorManager.java
@@ -1,7 +1,26 @@
+/*
+ * This file is part of "hybris integration" plugin for Intellij IDEA.
+ * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.intellij.idea.plugin.hybris.moduleDiagram;
 
 import com.intellij.diagram.DiagramBuilder;
 import com.intellij.diagram.DiagramColorManagerBase;
+import com.intellij.diagram.DiagramEdge;
 import com.intellij.diagram.DiagramNode;
 import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.Nullable;
@@ -25,5 +44,15 @@ public class ModuleDepDiagramColorManager extends DiagramColorManagerBase {
             return NON_CUSTOM_EXTENSIONS_COLOR;
         }
         return super.getNodeHeaderColor(builder, node);
+    }
+
+    @Override
+    public Color getEdgeColor(DiagramEdge edge) {
+        if (edge instanceof ModuleDepDiagramEdge) {
+            if (((ModuleDepDiagramEdge) edge).isCircular()) {
+                return INNER;
+            }
+        }
+        return super.getEdgeColor(edge);
     }
 }
