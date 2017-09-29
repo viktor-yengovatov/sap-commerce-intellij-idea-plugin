7a8f7d064c083f7c00ea76b4afd7e4f7c0269077
Eugene Kudelevsky <ekudel@gmail.com>
2017-09-29 19:53:22 +0300

    IIP-326 perform getComputerName() in background when collecting stats
    Signed-off-by: Evgenii Kudelevskii ekudel@gmail.com

--- src/com/intellij/idea/plugin/hybris/statistics/impl/StatsRequest.java
+++ src/com/intellij/idea/plugin/hybris/statistics/impl/StatsRequest.java
@@ -18,27 +18,93 @@
 
 package com.intellij.idea.plugin.hybris.statistics.impl;
 
+import com.intellij.idea.plugin.hybris.common.HybrisConstants;
+import com.intellij.openapi.util.Ref;
+import com.intellij.util.containers.ContainerUtil;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
 import java.util.concurrent.Callable;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Created by Martin Zdarsky-Jones (martin.zdarsky@hybris.com) on 28/2/17.
  */
 public class StatsRequest implements Callable<StatsResponse> {
 
-    private final HttpPost post;
-    private final HttpClient client;
+    private static final Object computerNameLock = new Object();
+    @Nullable
+    private static Ref<String> cachedComputerNameRef = null;
 
-    public StatsRequest(final HttpClient client, HttpPost post) {
+    @NotNull
+    private final HttpClient client;
+    @NotNull
+    private final List<NameValuePair> urlParameters;
+
+    public StatsRequest(@NotNull final HttpClient client, @NotNull final List<NameValuePair> urlParameters) {
         this.client = client;
-        this.post = post;
+        this.urlParameters = ContainerUtil.newArrayList(urlParameters);
     }
 
     @Override
     public StatsResponse call() throws Exception {
+        final List<NameValuePair> patchedUrlParameters = ContainerUtil.newArrayList(urlParameters);
+        patchUrlParameters(patchedUrlParameters);
+        final HttpPost post = new HttpPost(HybrisConstants.STATS_COLLECTOR_URL);
+        post.setEntity(new UrlEncodedFormEntity(patchedUrlParameters, UTF_8));
         return new StatsResponse(client.execute(post));
     }
 
+    protected void patchUrlParameters(@NotNull final List<NameValuePair> urlParameters) {
+        final String computerName = getComputerName();
+
+        if (computerName != null) {
+            urlParameters.add(new BasicNameValuePair("computer_name", DigestUtils.sha512Hex(computerName)));
+        }
+    }
+
+    //the same user can have multiple computers with identical license. This would affect the user counter.
+    protected static String getComputerName() {
+        synchronized (computerNameLock) {
+            if (cachedComputerNameRef == null) {
+                cachedComputerNameRef = Ref.create(computeComputerName());
+            }
+            return cachedComputerNameRef.get();
+        }
+    }
+
+    @Nullable
+    private static String computeComputerName() {
+        try {
+            final String result = InetAddress.getLocalHost().getHostName();
+            if (StringUtils.isNotEmpty(result)) {
+                return result.trim();
+            }
+        } catch (UnknownHostException e) {
+            // failed;  try alternate means.
+        }
+
+        // try environment properties.
+        final String envCompName = System.getenv("COMPUTERNAME");
+        if (envCompName != null) {
+            return envCompName;
+        }
+        final String host = System.getenv("HOSTNAME");
+        if (host != null) {
+            return host;
+        }
+
+        return null;
+    }
 }
