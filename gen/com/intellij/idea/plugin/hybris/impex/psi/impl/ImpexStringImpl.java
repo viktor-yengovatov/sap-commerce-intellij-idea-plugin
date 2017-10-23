9de15408ff195c0e42e91883b75d1c56e6d693a1
viatra2 <viatra2@gmail.com>
2017-10-23 02:45:22 +0200

    IIP-257: recognize FlexibleSearch laguage inside of Impex language
    Signed-off-by: viatra2 <viatra2@gmail.com>

--- gen/com/intellij/idea/plugin/hybris/impex/psi/impl/ImpexStringImpl.java
+++ gen/com/intellij/idea/plugin/hybris/impex/psi/impl/ImpexStringImpl.java
@@ -8,10 +8,9 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
 import static com.intellij.idea.plugin.hybris.impex.psi.ImpexTypes.*;
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.idea.plugin.hybris.impex.psi.*;
 
-public class ImpexStringImpl extends ASTWrapperPsiElement implements ImpexString {
+public class ImpexStringImpl extends ImpexStringMixin implements ImpexString {
 
   public ImpexStringImpl(ASTNode node) {
     super(node);
---
+++ src/com/intellij/idea/plugin/hybris/impex/psi/impl/ImpexStringMixin.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of "hybris integration" plugin for Intellij IDEA.
+ * Copyright (C) 2014-2016 Alexander Bartash <AlexanderBartash@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.intellij.idea.plugin.hybris.impex.psi.impl;
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.LiteralTextEscaper;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import org.jetbrains.annotations.NotNull;
+
+public class ImpexStringMixin extends ASTWrapperPsiElement implements PsiLanguageInjectionHost {
+
+    public ImpexStringMixin(@NotNull final ASTNode node) {
+        super(node);
+    }
+
+    @Override
+    public boolean isValidHost() {
+        return true;
+    }
+
+    @Override
+    public PsiLanguageInjectionHost updateText(@NotNull final String text) {
+        return this;
+    }
+
+    @NotNull
+    @Override
+    public LiteralTextEscaper<? extends PsiLanguageInjectionHost> createLiteralTextEscaper() {
+        return LiteralTextEscaper.createSimple(this);
+    }
+}
