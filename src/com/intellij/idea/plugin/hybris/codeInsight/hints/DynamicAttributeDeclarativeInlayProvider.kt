2c31bfa67b9d64f7027d8d2cd3109cc42488bdcd
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2023-07-20 10:09:37 +0200

    #521 | Improve performance of the inlay for `DynamicAttributeHandler`

--- src/com/intellij/idea/plugin/hybris/codeInsight/hints/DynamicAttributeDeclarativeInlayProvider.kt
+++ src/com/intellij/idea/plugin/hybris/codeInsight/hints/DynamicAttributeDeclarativeInlayProvider.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
- * Copyright (C) 2019 EPAM Systems <hybrisideaplugin@epam.com>
+ * Copyright (C) 2019-2023 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -23,6 +23,11 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiFile
 
 class DynamicAttributeDeclarativeInlayProvider : InlayHintsProvider {
-    override fun createCollector(file: PsiFile, editor: Editor) = if (HybrisProjectSettingsComponent.getInstance(file.project).isHybrisProject()) DynamicAttributeDeclarativeInlayHintsCollector()
+
+    private val collector by lazy {
+        DynamicAttributeDeclarativeInlayHintsCollector()
+    }
+
+    override fun createCollector(file: PsiFile, editor: Editor) = if (HybrisProjectSettingsComponent.getInstance(file.project).isHybrisProject()) collector
     else null
 }
