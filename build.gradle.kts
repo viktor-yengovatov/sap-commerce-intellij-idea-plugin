867d239fb3d39bb91e1bf627f08b0a13974dc52b
Mykhailo Lytvyn <lytvyn.michael@gmail.com>
2023-10-06 20:03:29 +0200

    #756 | Changelog should support 4+ nested versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 /*
  * This file is part of "SAP Commerce Developers Toolset" plugin for Intellij IDEA.
- * Copyright (C) 2019 EPAM Systems <hybrisideaplugin@epam.com>
+ * Copyright (C) 2019-2023 EPAM Systems <hybrisideaplugin@epam.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -72,7 +72,7 @@ idea {
 changelog {
     version = properties("intellij.plugin.version")
     groups = listOf()
-    headerParserRegex = """(\d+\.\d+(.\d+)?)""".toRegex()
+    headerParserRegex = """(\d+(.\d+)*)""".toRegex()
 }
 
 tasks {
